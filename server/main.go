81de1325d8ea766b0d97697c19d2ad1d28169e87
Roger Sen Montero <roger.sen@gmail.com>
2025-04-07 21:37:28 +0200

    updated docs and VERSION

--- server/main.go
+++ server/main.go
@@ -38,7 +38,7 @@ var (
 )
 
 const (
-	VERSION   = "5.4.1"
+	VERSION   = "5.5.1rc/multiple-web-hooks"
 	STRINGVER = "fujinet persistent lobby  " + VERSION + "/" + runtime.GOOS + " (c) Roger Sen 2025"
 )
 
